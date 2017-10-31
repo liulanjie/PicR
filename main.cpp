302ef5e3006efaf992dfaab2a0066e31881d8d9b
Zeno <liulanjie@foxmail.com>
2017-11-01 00:19:43 +0800

    完成抓包部分

--- main.cpp
+++ main.cpp
@@ -1,68 +1,114 @@
+/*
+ * @Copyrigth: CQUPT 
+ * @Author: Zeno 
+ * @Date: 2017-10-31 00:58:56 
+ * @Last Modified by: Zeno
+ * @Last Modified time: 2017-10-31 01:05:40
+ * @Description: 图片还原程序
+ */
+
 #include <stdio.h>
-#include "pcap.h"
 #include <stdlib.h>
-#include <vector>
+#include <stdint.h>
+#include<string.h>
+#include "pcap.h"
 
+#define MAX_PKT_NUM 65535
 
-int main()
+typedef struct _PKT_
 {
-    char *dev;                      /* Device to sniff on */
-    char errbuf[PCAP_ERRBUF_SIZE];  /* Error string */
-    pcap_t *handle;                 /* Session handle */  
-    struct bpf_program fp;          /* The compiled filter expression */
-    char filter_exp[] = "";  /* The filter expression */
-    bpf_u_int32 mask;               /* The netmask of our sniffing device */
-    bpf_u_int32 net;                /* The IP of our sniffing device */
-    struct pcap_pkthdr header;      /* The header that pcap gives us */
-    const u_char *packet;           /* The actual packet */
+    pcap_pkthdr *pHdr;
+    u_char *packet;
+} Pkt;
 
-    /* Define the device */
-    dev = pcap_lookupdev(errbuf);
-    if (NULL == dev)
+Pkt pkts[MAX_PKT_NUM];
+static uint16_t num = 0;
+
+int readCapture(char *file);
+int freeCapture();
+void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet);
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
     {
-        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
-        return(2);
+        printf("Please Enter the file name.\n");
+        return 0;
     }
-    printf("Device: %s\n", dev);
-
-    /* Find the properties for the device */
-    if (-1 == pcap_lookupnet(dev, &net, &mask, errbuf)) {
-        fprintf(stderr, "Can't get netmask for device %s\n", dev);
-        net = 0;
-        mask = 0;
-    }
-
-    /* Open the session in promiscuous mode */
-    handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-    if (NULL == handle)
+    else if (argc == 2)
     {
-        fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
-        return(2);
-    }    
-    if (pcap_datalink(handle) != DLT_EN10MB) {
-        fprintf(stderr, "Device %s doesn't provide Ethernet headers - not supported\n", dev);
-        return(2);
-    }
-
-    /* Compile and apply the filter */
-    if (NULL != filter_exp)
-    {
-        if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-            fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
-            return(2);
-        }
-        if (pcap_setfilter(handle, &fp) == -1) {
-            fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
-            return(2);
+        if (-1 == readCapture(argv[1]))
+        {
+            return 0;
         }
     }
+    else
+    {
+        printf("Too more params.\n");
+        return 0;
+    }
+    
+    //TODO Processer
 
-    /* Grab a packet */
-    packet = pcap_next(handle, &header);
-    /* Print its length */
-    printf("Jacked a packet with length of [%d]\n", header.len);
-    /* And close the session */
-    pcap_close(handle);
-
+    freeCapture();
+    
     return 0;
+}
+
+/**
+ * @brief 读包函数
+ * 
+ * @param file 文件名 
+ * @return int -1 失败， 0 成功
+ */
+int readCapture(char *file)
+{
+    pcap_t *handle;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    if (NULL == (handle = pcap_open_offline(file, errbuf)))
+    {
+        printf("Error: %s\n", errbuf);
+        return -1;
+    }
+    if (-1 == pcap_loop(handle, -1, callback, NULL))
+    {
+        printf("Error Capture format.\n");
+        return -1;
+    }
+    pcap_close(handle);
+}
+
+/**
+ * @brief 释放内存
+ * 
+ * @return int -1 失败， 0 成功
+ */
+int freeCapture()
+{
+    for (uint16_t ix = 0; ix < num; ++ix)
+    {
+        free(pkts[ix].pHdr);
+        free(pkts[ix].packet);
+    }
+    return 0;
+}
+
+/**
+ * @brief pcap_loop 回调函数
+ * 
+ * @param args 参数
+ * @param header pcap 包头 
+ * @param packet 包负载
+ */
+void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+    Pkt *pPkt = &pkts[++num];
+    size_t headerLen = sizeof(pcap_pkthdr);
+    size_t packetLen = sizeof(u_char) * header->len;
+
+    pPkt->pHdr = (pcap_pkthdr *)malloc(headerLen);
+    pPkt->packet = (u_char *)malloc(packetLen);
+
+    memcpy(pPkt->pHdr, header, headerLen);
+    memcpy(pPkt->packet, packet, packetLen);
 }
