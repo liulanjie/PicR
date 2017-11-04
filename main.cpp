706c35a42363afc5feb3289fba194690a291d077
Zeno <liulanjie@foxmail.com>
2017-11-04 22:23:52 +0800

    修复解析BUG，TCP建流

--- main.cpp
+++ main.cpp
@@ -10,15 +10,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include<string.h>
+#include <string.h>
 #include "pcap.h"
 
 #define MAX_PKT_NUM 65535
+#define MAX_HASH_NUM 4998
 #define ETHER_ADDR_LEN 6
 #define ETH_HDR_LEN 14
 
 #define ETHERTYPE_IP 8
 #define PROTOCOL_TCP 6
+
 //存放读出报文结构体
 typedef struct _PKT_
 {
@@ -32,45 +34,68 @@ typedef struct _PKT_
     uint32_t uDst;
     uint16_t uSrcPort;
     uint16_t uDstPort;
-} Pkt;
+}Pkt;
+
+//流节点
+typedef struct _HashNode_
+{
+    Pkt *pkt;
+    struct _HashNode_ *nextNode;
+}HashNode;
+
+//Hash 槽
+typedef struct _HashSlot_
+{
+    bool isUsed;
+    uint16_t nodeNum;
+    HashNode *firstNode;
+}HashSlot;
 
 //Eth 报头结构体
 typedef struct ether_header  
 {  
-    uint8_t ether_dhost[ETHER_ADDR_LEN];  
-    uint8_t ether_shost[ETHER_ADDR_LEN];  
-    uint16_t ether_type;  
+    uint8_t DstAddress[ETHER_ADDR_LEN];  
+    uint8_t SrcAddress[ETHER_ADDR_LEN];  
+    uint16_t Type;  
 }ETH_HEADER;
 
 //IP 报头结构体
-typedef struct ip  
-{  
-    uint8_t ip_v:4;
-    uint8_t ip_hl:4;
-    uint8_t ip_tos;
-    uint16_t ip_len;
-    uint16_t ip_id;
-    uint16_t ip_off;
-    uint8_t ip_ttl;
-    uint8_t ip_p;
-    uint16_t ip_sum;
-    uint32_t ip_src;
-    uint32_t ip_dst;
+typedef struct IPHeader {
+    uint8_t HeaderLen : 4;
+    uint8_t Version : 4;
+    uint8_t DS ;
+    uint16_t TotalLength;
+    uint16_t ID;
+    uint8_t FragmentOffset0 : 5;
+    uint8_t MF : 1;
+    uint8_t DF : 1;
+    uint8_t Reserved : 1;
+    uint8_t FragmentOffset1;
+    uint8_t TTL;
+    uint8_t Protocol;
+    uint16_t IpChkSum;
+    uint32_t SrcIP;
+    uint32_t DstIP;
 }IP_HEADER;
 
 //TCP 报头结构体
-typedef struct tcphdr   
-{  
-    uint16_t th_sport;
-    uint16_t th_dport;
-    uint32_t th_seq;
-    uint32_t th_ack;
-    uint8_t th_off:4;
-    uint8_t th_x2:4;
-    uint8_t th_flags;
-    uint16_t th_win;
-    uint16_t th_sum;
-    uint16_t th_urp;
+typedef struct TCPHEADER {
+    uint16_t SrcPort;
+    uint16_t DstPort;
+    uint32_t SeqNo;
+    uint32_t AckNo;
+    uint8_t Reserved0 : 4;
+    uint8_t HeaderLen : 4;
+    uint8_t FIN : 1;
+    uint8_t SYN : 1;
+    uint8_t RST : 1;
+    uint8_t PSH : 1;
+    uint8_t ACK : 1;
+    uint8_t URG : 1;
+    uint8_t Reserved1 : 2;
+    uint16_t WindowSize;
+    uint16_t TcpChkSum;
+    uint16_t UrgentPointer;
 }TCP_HEADER;
 
 //UDP 报头结构体
@@ -83,17 +108,25 @@ typedef struct udphdr
 }UDP_HEADER; 
 
 Pkt *pkts[MAX_PKT_NUM];
-static uint16_t num = 0;
-static uint16_t tcp_num;
+HashSlot *HashTable[MAX_HASH_NUM];
+static uint16_t pktNum = 0;
+static uint16_t tcp_num = 0;
 
 int readCapture(char *file);
 int freeCapture();
+void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *pStr);
+
+int initHashTable();
+int initHashSlot(HashSlot *pSlot);
+int initHashNode(HashNode *pNode);
+int pushNode(HashSlot *pSlot, HashNode *pNode);
+int popNode();
+
 void processer();
 int ethParse(Pkt &pkt);
 int ipParse(Pkt &pkt);
 int tcpParse(Pkt &pkt);
 int udpParse(Pkt &pkt);
-void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *pStr);
 
 //函数返回值
 namespace RET
@@ -121,11 +154,13 @@ int main(int argc, char *argv[])
         printf("Too more params.\n");
         return 0;
     }
+
+    initHashTable();
     
     processer();
 
     freeCapture();
-    
+
     return 0;
 }
 
@@ -161,7 +196,7 @@ int readCapture(char *file)
  */
 int freeCapture()
 {
-    for (uint16_t ix = 0; ix < num; ++ix)
+    for (uint32_t ix = 0; ix < pktNum; ++ix)
     {
         free(pkts[ix]->pHdr);
         free(pkts[ix]->pStr);
@@ -190,7 +225,7 @@ void callback(u_char *args, const struct pcap_pkthdr *header, const u_char *pStr
     memcpy(pPkt->pHdr, header, headerLen);
     memcpy(pPkt->pStr, pStr, pStrLen);
 
-    pkts[num++] = pPkt;
+    pkts[pktNum++] = pPkt;
 }
 
 /**
@@ -218,7 +253,7 @@ void showPkt(Pkt &pkt)
 void processer()
 {
     //协议解析
-    for (uint16_t ix = 0; ix < num; ++ix)
+    for (uint32_t ix = 0; ix < pktNum; ++ix)
     {
         Pkt &pkt = *pkts[ix];
 
@@ -228,20 +263,20 @@ void processer()
     printf("TCP NUM: %d\n", tcp_num);
 
     //TODO 建流
-    for (uint16_t ix = 0; ix < num; ++ix)
+    for (uint32_t ix = 0; ix < pktNum; ++ix)
     {
         Pkt &pkt = *pkts[ix];
-        //For Test
-        if(pkt.uType == 1)
-        {
-            printf("---------------------\n");
-            printf("SrcIP:%d\n", pkt.uSrc);
-            printf("DstIP:%d\n", pkt.uDst);
-            printf("SrcPort:%d\n", pkt.uSrcPort);
-            printf("DstPort:%d\n", pkt.uDstPort);
-        }
 
     }
+
+    //统计建流条数
+    int StreamNum = 0;
+    for(uint16_t ix = 0; ix != MAX_HASH_NUM; ++ix)
+    {
+        if (HashTable[ix]->isUsed == true)
+            ++StreamNum;
+    }
+    printf("STREAM NUM:%d\n", StreamNum);
 }
 
 /**
@@ -259,7 +294,7 @@ int ethParse(Pkt &pkt)
     ETH_HEADER *ethHdr = (ETH_HEADER *)pkt.pStr;
     pkt.uOffset += ETH_HDR_LEN;
 
-    if (ETHERTYPE_IP == ethHdr->ether_type)
+    if (ETHERTYPE_IP == ethHdr->Type)
         ipParse(pkt);
     else
         return RET::FAIL;
@@ -276,11 +311,11 @@ int ethParse(Pkt &pkt)
 int ipParse(Pkt &pkt)
 {
     IP_HEADER *ipHdr = (IP_HEADER *)(pkt.pStr + pkt.uOffset);
-    pkt.uOffset += ipHdr->ip_hl;
-    pkt.uDst = ipHdr->ip_dst;
-    pkt.uSrc = ipHdr->ip_src;
+    pkt.uOffset += (ipHdr->HeaderLen << 2);
+    pkt.uDst = ipHdr->DstIP;
+    pkt.uSrc = ipHdr->SrcIP;
 
-    if (PROTOCOL_TCP == ipHdr->ip_p)
+    if (PROTOCOL_TCP == ipHdr->Protocol)
         tcpParse(pkt);
     else
         return RET::FAIL;
@@ -300,10 +335,47 @@ int tcpParse(Pkt &pkt)
 
     TCP_HEADER *tcpHdr = (TCP_HEADER *)(pkt.pStr + pkt.uOffset);
     
-    pkt.uDstPort = tcpHdr->th_dport;
-    pkt.uSrcPort = tcpHdr->th_sport;
+    pkt.uDstPort = tcpHdr->DstPort;
+    pkt.uSrcPort = tcpHdr->SrcPort;
+    pkt.uOffset = (tcpHdr->HeaderLen << 2);
 
     pkt.uType = 1;
+
+    //TCP 建流
+    uint32_t uHashValue = pkt.uDstPort ^ pkt.uSrcPort ^ pkt.uSrc ^ pkt.uDst;
+    HashSlot &pSlot = *HashTable[uHashValue % MAX_HASH_NUM];
+
+    HashNode *pNode = (HashNode *)malloc(sizeof(HashNode));
+    pNode->pkt = &pkt;
+    pNode->nextNode = NULL;
+
+    if ((false == pSlot.isUsed) && (NULL == pSlot.firstNode))
+    {
+        //第一条报文，先压入
+        pushNode(&pSlot, pNode);
+
+        ++pSlot.nodeNum;
+    }
+    else if ((false == pSlot.isUsed) && (NULL != pSlot.firstNode))
+    {
+        //第二条报文，开始建流
+        pSlot.isUsed = true;
+        pushNode(&pSlot, pNode);
+        
+        ++pSlot.nodeNum;
+    }
+    else if (true == pSlot.isUsed)
+    {
+        //后续报文挂到流链表上
+        pushNode(&pSlot, pNode);
+
+        ++pSlot.nodeNum;
+    }
+    else
+    {
+        //其余情况异常
+        free(pNode);
+    }
 
     return RET::SUCCESS;
 }
@@ -315,6 +387,75 @@ int tcpParse(Pkt &pkt)
  * @return int -1 失败， 0 成功
  */
 int udpParse(Pkt &pkt)
+{
+    return RET::SUCCESS;
+}
+
+/**
+ * @brief 哈希表初始化
+ * 
+ * @return int 
+ */
+int initHashTable()
+{
+    for(uint16_t ix = 0; ix != MAX_HASH_NUM; ++ix)
+    {
+        HashTable[ix] = (HashSlot *)malloc(sizeof(HashSlot));
+
+        initHashSlot(HashTable[ix]);
+    }
+
+    return RET::SUCCESS;
+}
+
+/**
+ * @brief 哈希槽初始化
+ * 
+ * @param node 
+ * @return int 
+ */
+int initHashSlot(HashSlot *pSlot)
+{
+    pSlot->isUsed = false;
+    pSlot->firstNode = NULL;
+    pSlot->nodeNum = 0;
+
+    return RET::SUCCESS;
+}
+
+/**
+ * @brief 初始化哈希节点
+ * 
+ * @param node 
+ * @return int 
+ */
+int initHashNode(HashNode *pNode)
+{
+    pNode->pkt = NULL;
+    pNode->nextNode = NULL;
+
+    return RET::SUCCESS;
+}
+
+int pushNode(HashSlot *pSlot, HashNode *pNode)
+{
+    if (NULL == pSlot->firstNode)
+    {
+        pSlot->firstNode = pNode;
+    }
+    else
+    {
+        HashNode *qNode = pSlot->firstNode;
+        while(NULL != qNode->nextNode)
+        {
+            qNode = qNode->nextNode;
+        }
+        qNode->nextNode = pNode;
+    }
+
+    return RET::SUCCESS;
+}
+int popNode()
 {
     return RET::SUCCESS;
 }
